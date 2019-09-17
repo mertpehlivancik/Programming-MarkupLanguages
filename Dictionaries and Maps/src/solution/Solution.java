8041d53d8b44b44ef264d590598ea8bd47ff9110
Mert Pehlivancık <m.pehlivancik@outlook.com>
2019-09-17 12:11:19 +0300

    Add files via upload

---
+++ Dictionaries and Maps/src/solution/Solution.java
@@ -0,0 +1,29 @@
+package solution;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Map<String, Integer> myMap = new HashMap<String, Integer>();
+		for (int i = 0; i < n; i++) {
+			String name = in.next();
+			int phone = in.nextInt();
+			// Write code here
+			myMap.put(name, phone);
+		}
+
+		while (in.hasNext()) {
+			String s = in.next();
+			// Write code
+			if (myMap.get(s) == null) {
+				System.out.println("Not found");
+			} else {
+				System.out.println(s + "=" + myMap.get(s));
+			}
+		}
+		in.close();
+	}
+}
