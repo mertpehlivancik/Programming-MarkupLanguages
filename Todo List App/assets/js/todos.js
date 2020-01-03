21b36fbab763fda00ce498b9cf4ec43db006e8a9
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-03 15:07:43 +0300

    Items could be deleted in list.

--- Todo List App/assets/js/todos.js
+++ Todo List App/assets/js/todos.js
@@ -22,3 +22,10 @@ $("li").click(function(){
 	$(this).toggleClass("completed");
 });
 */
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropogation();
+});
