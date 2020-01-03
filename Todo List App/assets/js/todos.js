9d249b8a79daec667833440ea28e7a56d6743fee
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-03 11:20:01 +0300

    Click item event was updated.

---
+++ Todo List App/assets/js/todos.js
@@ -0,0 +1,23 @@
+// Check Off Specific Todos By Clicking
+$("li").click(function(){
+	// If li is gray
+	console.log($(this).css("color"));
+	if ($(this).css("color") === "rgb(255, 0, 0)") {
+		// Turn it black
+		$(this).css("color","black");
+		$(this).css("text-decoration","none");
+	}
+	else {
+		$(this).css({
+		color: "red",
+		textDecoration: "line-through"
+	});
+	}
+});
+
+/*Below code block makes same job above code block.*/
+
+/*
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
