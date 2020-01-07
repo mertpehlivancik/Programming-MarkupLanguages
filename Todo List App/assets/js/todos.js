8f8692f6415f5c3c0c62ffae49f439314f82af13
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-07 17:06:01 +0300

    Added items can be deleted.

--- Todo List App/assets/js/todos.js
+++ Todo List App/assets/js/todos.js
@@ -20,6 +20,7 @@ $("li").click(function(){
 
 
 /*Below code block makes same job above code block.*/
+/*
 $("li").click(function(){
 	$(this).toggleClass("completed");
 });
@@ -40,6 +41,27 @@ $("input[type='text']").keypress(function(event){
 		$("ul").append("<li><span>X</span> " + todoText + "</li>");
 	}
 });
+*/
 
+// Check Off Specific Todos By Clicking
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
 
+// Click on X to Delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropogation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// Grabbing new todo text from input
+		var todoText = $(this).val();
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+});
 
