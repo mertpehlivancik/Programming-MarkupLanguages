2422bbe9ad3e8c7f83b5ec0ed605fe4709d5fbd4
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-25 16:21:34 +0300

    Data was passed from js file to ejs file.

--- Node.js/Express Framework/EJS Demo/app.js
+++ Node.js/Express Framework/EJS Demo/app.js
@@ -1,7 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res){
+	//res.send("<h1>Welcome to the home page</h1>");
+	res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+	var thing = req.params.thing;
+	//res.send("You fell in love with " + thing);
+	res.render("love.ejs", {thingVar : thing});
+});
+
 app.listen(3000, "localhost", function(){
 	console.log("Server is listening!!!");
 });
-
