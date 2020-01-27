afe2715f6c0c71cff2ca039b9f5ffff79673dee2
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-27 18:33:19 +0300

    EJS: Conditionals and Loops was added.

--- Node.js/Express Framework/EJS Demo/app.js
+++ Node.js/Express Framework/EJS Demo/app.js
@@ -12,6 +12,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
 	res.render("love.ejs", {thingVar : thing});
 });
 
+app.get("/posts", function(req, res){
+	var posts = [
+		{title: "Post 1", author: "Susy"},
+		{title: "My adorable pet bunny", author: "Charlie"},
+		{title: "Can you believe this pomsky?", author: "Colt"}
+	];
+
+	res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, "localhost", function(){
 	console.log("Server is listening!!!");
 });
