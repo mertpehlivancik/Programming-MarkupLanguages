6cb43c905e27d9bbd94331a0874792195064dc17
MertPehlivancik <m.pehlivancik@gmail.com>
2020-01-28 16:31:47 +0300

    CSS, header and footer were added

--- Node.js/Express Framework/EJS Demo/app.js
+++ Node.js/Express Framework/EJS Demo/app.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
 	//res.send("<h1>Welcome to the home page</h1>");
-	res.render("home.ejs");
+	res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
 	var thing = req.params.thing;
 	//res.send("You fell in love with " + thing);
-	res.render("love.ejs", {thingVar : thing});
+	res.render("love", {thingVar : thing});
 });
 
 app.get("/posts", function(req, res){
@@ -19,7 +22,7 @@ app.get("/posts", function(req, res){
 		{title: "Can you believe this pomsky?", author: "Colt"}
 	];
 
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
 });
 
 app.listen(3000, "localhost", function(){
