ce13b1bbf9f851b9bc3f83f2bec3fb3ecdffb48b
MertPehlivancik <m.pehlivancik@gmail.com>
2020-02-04 14:31:31 +0300

    .

---
+++ Loops/solution/Solution.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
+        
+    a = 0
+    while a < n:
+        print(a**2)
+        a = a + 1 
+    
