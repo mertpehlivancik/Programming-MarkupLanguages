1d7b6703a5b0ba0997192756029c16780f46382f
MertPehlivancik <m.pehlivancik@gmail.com>
2020-02-07 17:09:21 +0300

    Request was sent to "http://www.google.com"

---
+++ JavaScript/Node.js/APIs/FirstRequest.js
@@ -0,0 +1,19 @@
+/* const request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+}); */
+
+var request = require('request');
+request('www.google.com', function(error, response, body){
+	if(error){
+		console.log("Something went wrong!");
+		console.log(error);
+	} else{
+		if (response.statusCode == 200){
+			// Worked!
+			console.log(body);
+		}
+	}
+});
