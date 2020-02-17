5cf9a6a301b6c3e1ac3586ec1728dbb45983f35a
MertPehlivancik <m.pehlivancik@gmail.com>
2020-02-17 18:04:06 +0300

    Styles was updated.

--- JavaScript/Node.js/YelpCamp/v1/app.js
+++ JavaScript/Node.js/YelpCamp/v1/app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-	{name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-4817872?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e8d4444255ae14f6da8c7dda793f7f1636dfe2564c704c7d2e78dd924ec35d_960.jpg&user=chulmin1700"},
-	{name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-surrounded-with-green-grass-field-1061640.jpg&fm=jpg"},
-	{name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c7d2e78dd924ec35d_960.jpg&user=Ben_Frieden"}
+	{name: "Salmon Creek", image: "https://media-cdn.tripadvisor.com/media/photo-s/13/b8/8a/28/olakira-camp-asilia-africa.jpg"},
+	{name: "Granite Hill", image: "https://media-cdn.tripadvisor.com/media/photo-s/13/b8/8a/28/olakira-camp-asilia-africa.jpg"},
+	{name: "Mountain Goat's Rest", image: "https://media-cdn.tripadvisor.com/media/photo-s/13/b8/8a/28/olakira-camp-asilia-africa.jpg"}
 ];
 
 app.get("/", function(req, res){
