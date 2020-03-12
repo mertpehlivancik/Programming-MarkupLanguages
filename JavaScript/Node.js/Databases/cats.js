61c1e15ed5c17ed390d103a20cb5fefb273d5b5a
MertPehlivancik <m.pehlivancik@gmail.com>
2020-03-12 16:15:46 +0300

    .

---
+++ JavaScript/Node.js/Databases/cats.js
@@ -0,0 +1,52 @@
+// npm install mongoose
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/cat_app");
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+// adding a new cat to the DB
+
+// var george = new Cat({
+//     name: "George",
+//     age: 11,
+//     temperament: "Grouchy"
+// });
+
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("SOMETHING WENT WRONG");
+//     }else{
+//         console.log("WE JUST SAVED A CAT TO THE DB:");
+//         console.log(cat);
+//     }
+// });
+
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function(err, cat){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(cat);
+    }
+});
+
+// Retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("OH NO, ERROR!");
+        console.log(err);
+    }else {
+        console.log("ALL THE CATS......");
+        console.log(cats);
+    }
+});
