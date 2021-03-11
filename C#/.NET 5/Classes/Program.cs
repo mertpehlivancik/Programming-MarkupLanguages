5f703cc7dcb4cd96927ee18e2c11a79695137f2b
Mert Pehlivancık <m.pehlivancik@gmail.com>
2021-03-11 11:55:34 +0300

    Bank Account project was added.

---
+++ C#/.NET 5/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Mert", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
