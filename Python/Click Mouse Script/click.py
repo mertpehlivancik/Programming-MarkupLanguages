75418333cc2a44d4459871df5b6c0ccd6e35173e
Mert Pehlivancık <mpehlivancik@gmail.com>
2022-03-30 13:19:22 +0300

    Mouse is clicked with using script

---
+++ Python/Click Mouse Script/click.py
@@ -0,0 +1,11 @@
+## Firstly, install pyautogui with running this code:
+## py.exe -m pip install pyautogui
+
+import pyautogui,time
+while True:
+    ##pyautogui.click(100,950)
+    ##pyautogui.click(500,1000)
+    ## pyautogui.click(X coordinate,Y coordinate)
+    ## For example; if screen is 1920x1080, upper left corner is x=0,y=0
+    pyautogui.click(1800,1000)
+    time.sleep(0.5)
